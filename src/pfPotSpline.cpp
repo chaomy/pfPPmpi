e5583f4acdc527de5efc805a988bd45d6037155a
chaomy <chaomy@umich.edu>
2018-05-05 15:07:45 -0400

    restore

--- src/pfPotSpline.cpp
+++ src/pfPotSpline.cpp
@@ -2,7 +2,7 @@
  * @Author: yangchaoming
  * @Date:   2017-10-23 14:04:42
  * @Last Modified by:   chaomy
- * @Last Modified time: 2018-05-04 17:22:00
+ * @Last Modified time: 2018-05-05 15:07:32
  */
 
 #include "pfHome.h"
@@ -50,8 +50,7 @@ void pfHome::setSplineVariables() {  // func -> ini
       mx = std::max(ff.yy[j], mx);
       nvars++;
     }
-    // double del = 2 * (mx - mn);  // be careful on the delta
-    double del = 0.01 * (mx - mn);
+    double del = 2 * (mx - mn);  // be careful on the delta
     for (int j : ff.rlxid) {
       lob.push_back(ff.yy[j] - del);
       hib.push_back(ff.yy[j] + del);
